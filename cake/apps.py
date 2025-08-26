1c662c3c3a1efbae6d6f9babc0354a9c22024c1a
fergoth <fergothew@gmail.com>
2025-08-26 12:21:24 +0300

    static and rename app

--- cake/apps.py
+++ cake/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class CustomCakeConfig(AppConfig):
+class CakeConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'custom_cake'
+    name = 'cake'
