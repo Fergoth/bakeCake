617706533e3f00b71c79c4410522452ab898a630
fergoth <fergothew@gmail.com>
2025-08-26 12:55:48 +0300

    base views

--- bakecake/urls.py
+++ bakecake/urls.py
@@ -17,6 +17,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from cake.views import index, profile
 urlpatterns = [
+    path('', index, name='index'),
+    path('profile/', profile, name='profile'),
     path('admin/', admin.site.urls),
 ]
