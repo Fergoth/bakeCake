5f7ffbe9e78a092c88fcca115750049f3181ade4
fergoth <fergothew@gmail.com>
2025-08-31 21:00:53 +0300

    del redunant

--- custom_user/views.py
+++ custom_user/views.py
@@ -28,8 +28,6 @@ def register_user(request):
                 'name': name
             }
         )
-        user.backend = 'custom_user.authentication.NoPasswordBackend'
-
         backend = 'custom_user.authentication.NoPasswordBackend'
         login(request, user, backend=backend)
 
